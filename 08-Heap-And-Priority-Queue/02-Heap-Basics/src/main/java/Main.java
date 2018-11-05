c2c89bbf775260a6f00a22887b9ee61e58689830
gyoomi <gyoomi0709@foxmail.com>
2018-11-05 18:17:31 +0800

    二叉堆

---
+++ 08-Heap-And-Priority-Queue/02-Heap-Basics/src/main/java/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright © 2018, TaoDing
+ * <p>
+ * All Rights Reserved.
+ */
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2018/11/5 15:36
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int time = 100000;
+
+    }
+}
