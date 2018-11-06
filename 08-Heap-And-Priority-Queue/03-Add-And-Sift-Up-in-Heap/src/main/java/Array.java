c121f32e5cdc9912ff1ace15c3e91991d34d3636
gyoomi <gyoomi0709@foxmail.com>
2018-11-06 09:47:37 +0800

    二叉堆-添加上浮

---
+++ 08-Heap-And-Priority-Queue/03-Add-And-Sift-Up-in-Heap/src/main/java/Array.java
@@ -0,0 +1,189 @@
+/**
+ * Copyright © 2018, TaoDing
+ * <p>
+ * All Rights Reserved.
+ */
+
+/**
+ * 动态数组底层实现
+ *
+ * @author Leon
+ * @version 2018/11/5 14:08
+ */
+public class Array<E> {
+
+    private E[] data;
+    private int size;
+
+    public Array(int capacity) {
+        data = (E[])new Object[capacity];
+        size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    /**
+     * 获取数组容量
+     *
+     * @return
+     */
+    public int getCapacity() {
+        return data.length;
+    }
+
+    /**
+     * 获取数组中的元素个数
+     *
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 在index索引的位置处插入一个新的元素e
+     *
+     * @param index
+     * @param e
+     */
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed, Required index >= 0 and <= size.");
+        }
+        if (size == data.length) {
+            resize(data.length << 1);
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed, index is illegal.");
+        }
+        return data[index];
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed, index is illegal.");
+        }
+        data[index] = e;
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 查询数组中元素e所占的索引，如果不存在的话返回-1
+     *
+     * @param e
+     * @return
+     */
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed.Index is illegal.");
+        }
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null; // Loitering object != memory leak
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+        return ret;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    /**
+     * 交换
+     *
+     * @param i
+     * @param j
+     */
+    public void swap(int i, int j) {
+        if (i < 0 || i >= size || j < 0 || j >= size) {
+            throw new IllegalArgumentException("");
+        }
+        E t = data[i];
+        data[i] = data[j];
+        data[j] = t;
+    }
+
+
+    @Override
+    public String toString(){
+
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
+        res.append('[');
+        for (int i = 0 ; i < size ; i ++) {
+            res.append(data[i]);
+            if (i != size - 1)
+                res.append(", ");
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+    /**
+     * 扩容
+     *
+     * @param newCapacity
+     */
+    private void resize(int newCapacity) {
+        E[] newData = (E[])new Object[newCapacity];
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+}
---
+++ 08-Heap-And-Priority-Queue/03-Add-And-Sift-Up-in-Heap/src/main/java/MaxHeap.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright © 2018, TaoDing
+ * <p>
+ * All Rights Reserved.
+ */
+
+/**
+ * 二叉堆：最大堆
+ *
+ * @author Leon
+ * @version 2018/11/5 15:37
+ */
+public class MaxHeap<E extends Comparable<E>> {
+
+    private Array<E> data;
+    public MaxHeap(int capacity) {
+        data = new Array<>(capacity);
+    }
+    public MaxHeap() {
+        data = new Array<>();
+    }
+
+    public int size() {
+        return data.getSize();
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的父亲节点的索引
+     *
+     * @param index
+     * @return
+     */
+    private int parent(int index) {
+        if (index == 0) {
+            throw new IllegalArgumentException("Index 0 does't exist parent");
+        }
+        return (index - 1) / 2;
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的左孩子的节点的索引
+     *
+     * @param index
+     * @return
+     */
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    /**
+     * 返回完全二叉树数组表示中，一个索引所表示的右
+     *
+     * @param index
+     * @return
+     */
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+
+    public void add(E e) {
+        data.addLast(e);
+        siftUp(data.getSize() - 1);
+    }
+
+    /**
+     * 上浮
+     *
+     * @param index
+     */
+    private void siftUp(int index) {
+        while (index > 0 && data.get(parent(index)).compareTo(data.get(index)) < 0) {
+            data.swap(index, parent(index));
+            index = parent(index);
+        }
+    }
+}
