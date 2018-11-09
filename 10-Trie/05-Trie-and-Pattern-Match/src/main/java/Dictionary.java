f5a20b8df123db962d345494e0157551e4e25a47
gyoomi <gyoomi0709@foxmail.com>
2018-11-09 14:41:49 +0800

    字典树-match

---
+++ 10-Trie/05-Trie-and-Pattern-Match/src/main/java/Dictionary.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright © 2018, TaoDing
+ * <p>
+ * All Rights Reserved.
+ */
+
+import java.util.TreeMap;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2018/11/9 10:58
+ */
+public class Dictionary {
+
+    private Node root;
+    public Dictionary() {
+        root = new Node();
+    }
+
+    public void addWord(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+        }
+    }
+
+    public boolean search(String word) {
+        return match(root, word, 0);
+    }
+
+    private boolean match(Node node, String word, int index) {
+        if (index == word.length()) {
+            return node.isWord;
+        }
+        char c = word.charAt(index);
+        if (c != '.') {
+            if (node.next.get(c) == null) {
+                return false;
+            }
+            return match(node.next.get(c), word, index + 1);
+        } else {
+            for (Character nextChar : node.next.keySet()) {
+                if (match(node.next.get(nextChar), word, index + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+        public Node (boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+        public Node() {
+            this(false);
+        }
+    }
+}
---
+++ 10-Trie/05-Trie-and-Pattern-Match/src/main/java/Trie208.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright © 2018, TaoDing
+ * <p>
+ * All Rights Reserved.
+ */
+
+import java.util.TreeMap;
+
+/**
+ * Leetcode-208
+ *
+ * @author Leon
+ * @version 2018/11/9 10:34
+ */
+public class Trie208 {
+
+
+    private Node root;
+    private int size;
+
+    public Trie208() {
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void insert(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+    public boolean search(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+        public Node() {
+            this(false);
+        }
+    }
+}
