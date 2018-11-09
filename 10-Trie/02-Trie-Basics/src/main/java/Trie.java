fcd4d2c63afb01c8bd0866d1cbb3edce2c9cd2a0
gyoomi <gyoomi0709@foxmail.com>
2018-11-09 09:58:02 +0800

    字典树-basics-add

--- 10-Trie/02-Trie-Basics/src/main/java/Trie.java
+++ 10-Trie/02-Trie-Basics/src/main/java/Trie.java
@@ -26,6 +26,20 @@ public class Trie {
         return size;
     }
 
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
 
     private class Node {
         public boolean isWord;
