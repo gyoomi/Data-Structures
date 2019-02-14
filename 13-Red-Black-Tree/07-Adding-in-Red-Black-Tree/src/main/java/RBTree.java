525a3f61ae6bdb45099a7cff3bc7a95eda96951d
gyoomi <gyoomi0709@foxmail.com>
2019-02-14 18:22:23 +0800

    red

--- 13-Red-Black-Tree/07-Adding-in-Red-Black-Tree/src/main/java/RBTree.java
+++ 13-Red-Black-Tree/07-Adding-in-Red-Black-Tree/src/main/java/RBTree.java
@@ -4,8 +4,6 @@
  * All Rights Reserved.
  */
 
-import java.util.ArrayList;
-
 /**
  * 类功能描述
  *
@@ -281,26 +279,9 @@ public class RBTree<K extends Comparable<K>, V> {
     }
 
     public static void main(String[] args){
-
-        System.out.println("Pride and Prejudice");
-
-        ArrayList<String> words = new ArrayList<>();
-        if(FileOperation.readFile("d:\\pride-and-prejudice.txt", words)) {
-            System.out.println("Total words: " + words.size());
-
-            RBTree<String, Integer> map = new RBTree<>();
-            for (String word : words) {
-                if (map.contains(word))
-                    map.set(word, map.get(word) + 1);
-                else
-                    map.add(word, 1);
-            }
-
-            System.out.println("Total different words: " + map.getSize());
-            System.out.println("Frequency of PRIDE: " + map.get("pride"));
-            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
-        }
-
-        System.out.println();
+        RBTree<Integer, Object> rbt = new RBTree<>();
+        rbt.add(40, null);
+        rbt.add(43, null);
+        System.out.println(rbt);
     }
 }
