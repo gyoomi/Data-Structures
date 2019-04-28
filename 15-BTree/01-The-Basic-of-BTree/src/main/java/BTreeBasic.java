240c0005ec81287d8aa91663da6ffec89d25f910
leon <gyoomi0709@foxmail.com>
2019-04-28 23:48:27 +0800

    basic of b-tree

---
+++ 15-BTree/01-The-Basic-of-BTree/src/main/java/BTreeBasic.java
@@ -0,0 +1,16 @@
+/**
+ * B-tree树即B树，B即Balanced，平衡的意思。因为B树的原英文名称为B-tree.
+ *
+ * 是一种多路搜索树（并不是二叉的）。
+ *
+ *
+ *
+ * @author Leon
+ * @version 2019/4/28 22:50
+ */
+public class BTreeBasic {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
